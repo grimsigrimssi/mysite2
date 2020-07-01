48126eaff6c1e7862a2c1da72b994171ee134f06
'spark' <'grimsigrimssi@naver.com'>
2020-07-01 13:25:26 +0900

    4th

--- mysite2/src/com/javaex/vo/BoardVo.java
+++ mysite2/src/com/javaex/vo/BoardVo.java
@@ -3,45 +3,24 @@ package com.javaex.vo;
 public class BoardVo {
 	
 	private int no;
-	private String name;
-	private String password;
 	private String title;
 	private String content;
-	private String date;
-	private int count;
-	private String status;
+	private String name;
+	private int hit;
+	private String reg_date;
+	private int user_no;
 	
-	public BoardVo() {
-		
-	}
-	
-	public BoardVo(int no, String password, String title, String content) {
-		this.no = no;
-		this.password = password;
-		this.title = title;
-		this.content = content;
-	}
-	
-	public BoardVo(int no, String title, String name, int count, String date, String status) {
-		this.no = no;
-		this.title = title;
-		this.name = name;
-		this.count = count;
-		this.date = date;
-		this.status = status;
-	}
-		
-	public BoardVo(int no, String name, String password, String title, String content, String date, int count, String Status) {
-		this.no = no;
-		this.name = name;
-		this.password = password;
-		this.title = title;
-		this.content = content;
-		this.date = date;
-		this.count = count;
-		this.status = status;
-	}
+	public BoardVo() {}
 
+	public BoardVo(int no, String title, String content, String name, int hit, String reg_date, int user_no) {
+		this.no = no;
+		this.title = title;
+		this.content = content;
+		this.name = name;
+		this.hit = hit;
+		this.reg_date = reg_date;
+		this.user_no = user_no;
+	}
 
 	public int getNo() {
 		return no;
@@ -49,22 +28,6 @@ public class BoardVo {
 
 	public void setNo(int no) {
 		this.no = no;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 
 	public String getTitle() {
@@ -83,37 +46,42 @@ public class BoardVo {
 		this.content = content;
 	}
 
-	public String getDate() {
-		return date;
+	public String getName() {
+		return name;
 	}
 
-	public void setDate(String date) {
-		this.date = date;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public int getCount() {
-		return count;
+	public int getHit() {
+		return hit;
 	}
 
-	public void setCount(int count) {
-		this.count = count;
-	}
-	
-	public String getStatus() {
-		return status;
+	public void setHit(int hit) {
+		this.hit = hit;
 	}
 
-	public void setStatus(String status) {
-		this.status = status;
+	public String getReg_date() {
+		return reg_date;
+	}
+
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+
+	public int getUser_no() {
+		return user_no;
+	}
+
+	public void setUser_no(int user_no) {
+		this.user_no = user_no;
 	}
 
 	@Override
 	public String toString() {
-		return "BoardVo [no=" + no + ", name=" + name + ", password=" + password + ", title=" + title + ", content="
-				+ content + ", date=" + date + ", count=" + count + ", status=" + status + "]";
+		return "BoardVo [no=" + no + ", title=" + title + ", content=" + content + ", hit=" + hit + ", reg_date="
+				+ reg_date + ", user_no=" + user_no + "]";
 	}
 	
-	
-	
-
 }
